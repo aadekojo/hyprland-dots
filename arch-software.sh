110b649adc2f0f1fbdbf42dc5672e3d65d390f99
aadekojoa <adekojo.abimbola@gmail.com>
2024-12-16 18:43:50 -0600

    update

---
+++ arch-software.sh
@@ -0,0 +1,67 @@
+#!/usr/bin/env bash
+#-------------------------------------------------------------------------------------
+#     ___              __       _____       ______                              
+#    /   |  __________/ /_     / ___/____  / __/ /__      ______ ______________ 
+#   / /| | / ___/ ___/ __ \    \__ \/ __ \/ /_/ __/ | /| / / __ `/ ___/ ___/ _ \
+#  / ___ |/ /  / /__/ / / /   ___/ / /_/ / __/ /_ | |/ |/ / /_/ / /  / /  /  __/
+# /_/  |_/_/   \___/_/ /_/   /____/\____/_/  \__/ |__/|__/\__,_/_/  /_/   \___/                                                                             
+#  Arch Linux Post Install Setup and Config
+#-------------------------------------------------------------------------------------
+
+echo
+echo "INSTALLING SOFTWARE.........."
+echo
+
+PKGS=( 
+    # SYSTEM --------------------------------------------------------------
+
+    # 'linux-lts'             # Long term support kernel
+
+    # TERMINAL UTILITIES --------------------------------------------------
+    'curl'                  # Remote content retrieval
+    'fastfetch'             # Shows system info when you launch terminal
+    'openssh'               # SSH connectivity tools
+    'unzip'                 # Zip compression program
+    'kitty'                 # Terminal Emulator 
+
+    # DISK UTILITIES ------------------------------------------------------
+    'gparted'               # Disk utility
+
+    # DEVELOPMENT ---------------------------------------------------------
+    'clang'                 # C Lang compiler
+    'cmake'                 # Cross-platform open-source make system
+    'git'                   # Version control system
+
+    # MEDIA ---------------------------------------------------------------
+    'vlc'                   # Video player
+
+    # PRODUCTIVITY --------------------------------------------------------
+    'gwenview'              #image viewer
+    'thunar'                #file explorer
+    'thunar-archive-plugin' #thunar helper
+    'thunar-volman'         #thunar helper
+
+    #FONTS ----------------------------------------------------------------
+    'ttf-font-awesome'      #font awesonme
+)
+
+for PKG in "${PKGS[@]}"; do
+    echo "INSTALLING: ${PKG}"
+    sudo pacman -S "$PKG" --noconfirm --needed
+done
+
+echo
+echo "Installed Official Programs"
+echo
+
+echo
+echo "INSTALLING AUR SOFTWARE"
+echo
+
+cd "${HOME}"
+mkdir -p packages
+git clone "https://aur.archlinux.org/yay.git"
+cd yay
+makepkg -si
+
+echo "This is a test and nothing was installed after yay"
